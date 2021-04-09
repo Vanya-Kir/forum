444f2efce5ba5b40d6be1e0efd46c15936c19d35
Nikita Muzykantov <muzykantov99@gmail.com>
2021-04-09 14:35:43 +0300

    Update settings.py
    secret key now takes from env variable TOKEN

--- forum/settings.py
+++ forum/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ub)repcp&y)tndfqzbtgng%d-7m$wgb1p&edl=2yiwbh!t5uoc'
+SECRET_KEY = os.environ['TOKEN']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
