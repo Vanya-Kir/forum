e3138e5cd7303a1a02f04011940e949c3ae092f4
Nikita Muzykantov <muzykantov99@gmail.com>
2021-04-09 21:04:17 +0300

    added 'chuvsu.codes' to ALLOWED_HOSTS

--- forum/settings.py
+++ forum/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('TOKEN', 'ub)repcp&y)tndfqzbtgng%d-7m$wgb1p&edl=2yiw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com', '.chuvsu.codes']
 
 
 # Application definition
